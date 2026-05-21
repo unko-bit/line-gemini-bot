d543d4d7782c7ef9e4b358ffc299846d8ecc1be6
unko-bit <riku.idogawa2011@gmail.com>
2026-05-21 19:56:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,6 @@ def handle_message(event):
         )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # OSの環境変数からポートを取得し、なければ10000にする
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
